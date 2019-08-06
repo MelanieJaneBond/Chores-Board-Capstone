66dca8735227b47196ccbb2e9352c3d96bc411bf
MelanieJaneBond <miss.bond2@gmail.com>
2019-08-06 11:17:23 -0500

    cleared database of unwanted items

--- src/components/welcome/Message.js
+++ src/components/welcome/Message.js
@@ -9,9 +9,11 @@ export default class WelcomeMessage extends Component {
             <section className="card bg-danger mb-3">
                 <h1 className="message-end">The Chores Board</h1>
                     <div className="card-body">
-                        <p className="card-text">Climb out of your filth and grime;</p>
-                        <p className="card-text">get your chores done on time</p>
-                        <p className="card-text">with <strong>The Chores Board</strong> !</p>
+                        <div id="middle-words">
+                        <p>Climb out of your filth and grime;</p>
+                        <p>get your chores done on time</p>
+                        <p>with <strong>The Chores Board</strong> !</p>
+                        </div>
                         <div className="message-end">
                             <h3 className="card-title">Sign in or Create an Account :</h3>
                             <button className="btn btn-outline-dark" onClick={() => this.props.history.push(`/welcome`)}>Here</button>
